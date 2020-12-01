653c87dade0515d64a0cab6bbb8c43147050e1f2
analagossai <anala.gossai@gmail.com>
2020-11-30 20:02:27 -0500

    Session info added to start

--- AdventOfCode2020/advent_functions_2020.R
+++ AdventOfCode2020/advent_functions_2020.R
@@ -10,3 +10,29 @@ library(dplyr)
 
 ### test test test ................ 
 
+
+# Session Info ==================== 
+
+# R version 3.4.1 (2017-06-30)
+# Platform: x86_64-apple-darwin15.6.0 (64-bit)
+# Running under: macOS  10.16
+# 
+# Matrix products: default
+# LAPACK: /Library/Frameworks/R.framework/Versions/3.4/Resources/lib/libRlapack.dylib
+# 
+# locale:
+#   [1] en_US.UTF-8/en_US.UTF-8/en_US.UTF-8/C/en_US.UTF-8/en_US.UTF-8
+# 
+# attached base packages:
+#   [1] stats     graphics  grDevices utils     datasets 
+# [6] methods   base     
+# 
+# other attached packages:
+#   [1] dplyr_0.8.0.1
+# 
+# loaded via a namespace (and not attached):
+#   [1] tidyselect_0.2.5 compiler_3.4.1   magrittr_1.5    
+# [4] assertthat_0.2.0 R6_2.4.0         tools_3.4.1     
+# [7] pillar_1.3.1     glue_1.3.1       rstudioapi_0.9.0
+# [10] tibble_2.0.1     crayon_1.3.4     Rcpp_1.0.0      
+# [13] pkgconfig_2.0.2  rlang_0.3.1      purrr_0.2.3     
